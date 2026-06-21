fb0b8c03af043ef0b6cea822be60232bea5ee978
Julius Marminge <julius@mac.lan>
2026-06-20 19:21:58 -0700

    Complete orchestration V2 application services

--- apps/server/src/persistence/Migrations.ts
+++ apps/server/src/persistence/Migrations.ts
@@ -52,10 +52,12 @@ import Migration0037 from "./Migrations/037_ProjectionTurnsKeysetIndex.ts";
 import Migration0038 from "./Migrations/038_ProjectionThreadsPinOrderKey.ts";
 import Migration0039 from "./Migrations/039_ProjectionProjectsDefaultThreadEnvMode.ts";
 import Migration0040 from "./Migrations/040_ProjectionProjectFaviconPath.ts";
+import Migration037V2 from "./Migrations/037_OrchestrationV2ThreadLaunchWorkflows.ts";
 import Migration033V2 from "./Migrations/033_OrchestrationV2.ts";
 import Migration034V2 from "./Migrations/034_OrchestrationV2Subagents.ts";
 import Migration035V2 from "./Migrations/035_OrchestrationV2Foundation.ts";
 import Migration036V2 from "./Migrations/036_OrchestrationV2ProviderSessionBindings.ts";
+import Migration037V2 from "./Migrations/037_OrchestrationV2ThreadLaunchWorkflows.ts";
 
 /**
  * Migration loader with all migrations defined inline.
@@ -112,6 +114,7 @@ const migrationEntries = [
   [42, "OrchestrationV2Subagents", Migration034V2],
   [43, "OrchestrationV2Foundation", Migration035V2],
   [44, "OrchestrationV2ProviderSessionBindings", Migration036V2],
+  [45, "OrchestrationV2ThreadLaunchWorkflows", Migration037V2],
 ] as const;
 
 export const migrationManifest = migrationEntries.map(([id, name]) => [id, name] as const);
