29e9c3c3954cbf692408182e8d279eeb7b6895f8
Julius Marminge <julius0216@outlook.com>
2026-09-05 01:25:30 -0700

    test(web): clean up highlight fixture frames

--- apps/web/src/components/files/fileEditorHighlight.test.ts
+++ apps/web/src/components/files/fileEditorHighlight.test.ts
@@ -56,6 +56,7 @@ interface HeldResponse {
   data: WorkerResponse;
   deliver: () => void;
 }
+const animationFrames = new Set<ReturnType<typeof setImmediate>>();
 let responses: HeldResponse[];
 let responseWaiters: ((response: HeldResponse) => void)[];
 let terminationPromises: Promise<number>[];
